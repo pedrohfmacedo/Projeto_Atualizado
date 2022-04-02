2ea6aa04a095bea55bfd51a18306d8157a541dc9
pedrohfmacedo <101015333+pedrohfmacedo@users.noreply.github.com>
2022-04-02 16:48:31 -0300

    Add files via upload

--- Cordas.h
+++ Cordas.h
@@ -8,7 +8,7 @@ class Cordas : public Produto {
 	   int qtdCordas;
 	   
 	   public:
-       Cordas(string = "", string = "", string = "", float = 0.0, float = 0.0, int = 0, int qtdCordas = 0);
+       Cordas(string = "", string = "", string = "", float = 0.0, float = 0.0, int = 0, int  = 0);
 	   void set_qtdCordas(int);
 	   int get_qtdCordas();
 	   void imprime_cordas();
