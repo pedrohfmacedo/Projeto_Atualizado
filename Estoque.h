c2e479a194882119b984e74609ad46ffa14756af
pedrohfmacedo <101015333+pedrohfmacedo@users.noreply.github.com>
2022-04-02 16:49:22 -0300

    Add files via upload

--- Estoque.h
+++ Estoque.h
@@ -7,47 +7,51 @@
 class Estoque
 {   // Definindo vectors
 	vector <Cordas> Lista_Cordas;
-	vector <Percussao> Lista_Percurssao;
+	vector <Percussao> Lista_Percussao;
 	// Método para ler produto
 	void lerCordas();
 	void lerPercurssao();
 
 public:
+	Estoque();
+	~Estoque();
     // Método para inserir produto
-    bool inserirPercussao(Percussao); //
+    bool inserirPercussao(Percussao);
     bool inserirCordas(Cordas);
 
     // Método para remover produto
-    bool removerPercussao(string); //
+    bool removerPercussao(string); 
     bool removerCordas(string);
 
     // Métodos para pesquisar produto
-    bool pesquisarPercussao(string) const; //
-    bool pesquisarCordas(string) const;
-
+    bool pesquisarPercussao(string) ; 
+    bool pesquisarCordas(string) ;
 
     // Método para salvar produto
-    bool salvarPercussao();					//
-    bool salvarCordas();
-
+    void salvarPercussao();					
+    void salvarCordas();
+	// Método para atualizar quantidade no estoque 
 	bool set_SaidaEstoquePercussao(string, int);
 	bool set_EntradaEstoquePercussao(string, int);
 	bool set_SaidaEstoqueCordas(string,  int);
 	bool set_EntradaEstoqueCordas(string, int);
 
 	// Método para imprimir produto
-	void imprimePercussao() const; //
-	void imprimeCordas() const;
+	void imprimePercussao(); //
+	void imprimeCordas();
 
 	// Método para ordenar produto
     void ordenaPercussao(); //
     void ordenaCordas();
-
+    
+    bool verifica_Percussao();
+    bool verifica_Cordas();
+    
 	int get_indicePercussao(string) const; //
 	int get_indiceCordas(string) const;
 	
-	vector <Percussao> get_ListaPercussao() const;
-	vector <Cordas> get_ListaCordas() const;
-
+	Percussao get_ListaPercussao(string);
+	Cordas  get_ListaCordas(string);
+	
 };
 #endif
